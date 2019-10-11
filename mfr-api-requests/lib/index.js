f30dfd1e0f63e9c593d8ad8e38a646c638f4fabe
biniyama <biniyamasfaw@gmail.com>
2019-10-11 15:14:37 +0300

    Add try...catch block to inserting into DHIS2

--- mfr-api-requests/lib/index.js
+++ mfr-api-requests/lib/index.js
@@ -717,19 +717,24 @@ function setupApp () {
             "id": parent_id
           }
         }
-        var insert_detail = await fetch(mediatorConfig.config.DHIS2baseurl + organisationUnit_req, {
-          method: "POST",
-          headers: {
-            "Authorization":"Basic " + encodedDHIS2,
-            "Content-Type":"application/json"
-          },
-          body: JSON.stringify(organisationUnit_to_add)
-          
-        })
-        .then(response => response.json())
-        .then(function handleData(data) {
-          return_data = data;
-        })
+        try{
+          var insert_detail = await fetch(mediatorConfig.config.DHIS2baseurl + organisationUnit_req, {
+            method: "POST",
+            headers: {
+              "Authorization":"Basic " + encodedDHIS2,
+              "Content-Type":"application/json"
+            },
+            body: JSON.stringify(organisationUnit_to_add)
+            
+          })
+          .then(response => response.json())
+          .then(function handleData(data) {
+            return_data = data;
+          })
+        } catch(err) {
+          console.log("Register Organisation unit info: " + err)
+          return
+        }
         //console.log(return_data.response.errorReports)
         //responseBody = JSON.stringify(return_data);
         if(return_data.response.uid) {
